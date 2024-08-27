bb6a837c0083fa83a2b34f20ae7d4e3fb7bc492c
Ryan <coap.ryanmontgomery@gmail.com>
2024-08-27 01:49:09 -0600

    new

--- server/models.py
+++ server/models.py
@@ -20,22 +20,24 @@ db = SQLAlchemy(metadata=metadata)
 class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
 
+    # don't forget that every tuple needs at least one comma!
+    serialize_rules = ('-animals.zookeeper',)
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     birthday = db.Column(db.Date)
 
     animals = db.relationship('Animal', back_populates='zookeeper')
-    serialize_rules = ('-animals.zookeeper',)
 
 
 class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
 
+    serialize_rules = ('-animals.enclosure',)
+
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
     open_to_visitors = db.Column(db.Boolean)
-
-    serialize_rules = ('-animals.enclosure',)
 
     animals = db.relationship('Animal', back_populates='enclosure')
 
@@ -43,11 +45,11 @@ class Enclosure(db.Model, SerializerMixin):
 class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
 
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     species = db.Column(db.String)
-
-    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
 
     zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
     enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
